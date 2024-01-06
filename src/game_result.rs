a55e45e063f7430575365a7d17067f4fd0a59678
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-01-06 13:41:28 +0100

    Add conversion from GameResult to takparse::GameResult

--- src/game_result.rs
+++ src/game_result.rs
@@ -1,4 +1,4 @@
-use takparse::Color;
+use takparse::{Color, WinReason};
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub enum GameResult {
@@ -19,4 +19,35 @@ pub enum Reason {
     ReservesDepleted,
     BoardFill,
     ReversiblePlies,
+}
+
+impl From<Reason> for WinReason {
+    fn from(reason: Reason) -> Self {
+        match reason {
+            Reason::Road => Self::Road,
+            Reason::BoardFill | Reason::ReservesDepleted => Self::Flat,
+            Reason::ReversiblePlies => Self::Other,
+        }
+    }
+}
+
+pub struct Ongoing;
+
+impl TryFrom<GameResult> for takparse::GameResult {
+    type Error = Ongoing;
+
+    fn try_from(result: GameResult) -> Result<Self, Self::Error> {
+        Ok(match result {
+            GameResult::Ongoing => return Err(Ongoing),
+            GameResult::Draw { .. } => Self::Draw,
+            GameResult::Winner {
+                color: Color::White,
+                reason,
+            } => Self::White(reason.into()),
+            GameResult::Winner {
+                color: Color::Black,
+                reason,
+            } => Self::Black(reason.into()),
+        })
+    }
 }
