6b1a89b18ce177fdd3f5072effc61c7cf5329b10
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2025-12-23 10:51:09 +0100

    Make Colors and Stack public

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,13 @@ mod tps;
 mod wins;
 
 pub use board::Board;
+pub use colors::Colors;
 pub use error::{PlayError, StackError, TakeError};
 pub use game::Game;
 pub use game_result::GameResult;
 pub use move_gen::perf_count;
 pub use reserves::Reserves;
+pub use stack::Stack;
 pub use symm::Symmetry;
 pub use takparse;
 pub use wins::MAX_REVERSIBLE_PLIES;
